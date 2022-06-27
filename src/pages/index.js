bcbe82902b1fba5a055c29f50fcb60fab45df950
zzerowin <alaykr123@gmail.com>
2022-06-27 16:45:47 +0900

    테스트 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,10 @@ import React from "react"
 const IndexPage = () => {
 
   return (
-    <div>
-      Hello World!
-    </div>
+    <main>
+      <title>메인페이지</title>
+      <h1>메인페이지 입니다</h1>
+    </main>
   )
 }
 
