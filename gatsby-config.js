398982bab7a2a56ae81dd6ef4b260deea46ebc53
zzerowin <alaykr123@gmail.com>
2022-06-28 15:07:03 +0900

    블로그 페이지 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `My Blog 2022 06 27`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="../images/clifford.jpeg"
+        src="../images/KakaoTalk_Photo_2022-06-28-14-44-51.jpeg"
       />
     </Layout>
   );
