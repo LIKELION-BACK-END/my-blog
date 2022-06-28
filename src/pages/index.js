d4010d5497cccc51db0f4126a0f37670e95b176e
zzerowin <alaykr123@gmail.com>
2022-06-28 11:43:35 +0900

    StaticImage 컴포넌트로 로컬 이미지 보여주기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
-import React from "react"
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
-
   return (
-    <Layout>
-      <title>메인페이지</title>
-      <h1>메인페이지 입니다</h1>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpeg"
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
