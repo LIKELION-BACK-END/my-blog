abc5f1b6b1a9f95d9be9bac6390e81522e28e462
zzerowin <alaykr123@gmail.com>
2022-06-28 10:34:06 +0900

    컴포넌트 추가하여, 쓸데없는 소스코드 중복제거

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Head from "../components/head"
 
 function AboutPage() {
   return (
     <main>
       <title>자기소개</title>
-      <h1>자기소개 페이지 입니다</h1>
+      <Head />
+      <h1>자기 소개 페이지 입니다.</h1>
     </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Head from "../components/head";
 
 const IndexPage = () => {
 
   return (
     <main>
       <title>메인페이지</title>
+      <Head />
       <h1>메인페이지 입니다</h1>
     </main>
   )
